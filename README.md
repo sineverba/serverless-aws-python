75276b584763b2444c37875f3c3b9f2b8a12b2ef
sineverba <alessandro.arici@gmail.com>
2020-09-30 21:45:35 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Serverless AWS Python Example
+
+## Setup
+
+```shell
+
+$ python3 -m venv env # create a virtual env
+$ npm install
+
+```
+
+### Serverless main command
+
+```shell
+(env) $ serverless deploy # deploy to aws
+(env) $ sls offline # launch offline deploy
+```
---
+++ ping.py
@@ -0,0 +1,23 @@
+import json
+
+def main(event,context):
+
+    response = {
+        'message': 'success'
+    }
+
+    print(response)
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,GET',
+            'Access-Control-Allow-Credentials': 'true'
+        },
+        'body': json.dumps(response)
+    }
+
+if __name__ == "__main__":
+    main('', '')
